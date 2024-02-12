c91319dccd018b3382b5ffd568eeeb2749490e58
Daniil Pinchuk <JeetWork168@gmail.com>
2024-02-12 22:13:06 +0300

    feat:add Footer,adaptive for Footer,test done

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,13 +3,16 @@ import './footer.css';
 import { Button, Layout } from 'antd';
 import { CardComponent } from '@components/common/card-content';
 import { AndroidFilled, AppleFilled } from '@ant-design/icons';
+import { collapsedType } from 'src/models/types';
 
 const { Footer } = Layout;
 
-
-export const FooterComponent: React.FC = () => {
+export const FooterComponent: React.FC<collapsedType> = ({ collapsed }) => {
     return (
-        <Footer className='footer-layout' style={{ background: 'transparent' }}>
+        <Footer
+            className={collapsed ? 'footer-layout' : 'footer-layout_collapsed footer-layout'}
+            style={{ background: 'transparent' }}
+        >
             <Button type='link'>Смотреть отзывы</Button>
             <CardComponent
                 tag='footer-card'
@@ -27,7 +30,7 @@ export const FooterComponent: React.FC = () => {
                 additionalContent={
                     <Button style={{ color: '#000000D9' }} type='link'>
                         <AppleFilled />
-                        Android OS
+                        Android iOS
                     </Button>
                 }
             />
