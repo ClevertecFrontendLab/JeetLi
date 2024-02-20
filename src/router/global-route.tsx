3a2bf6db18b9e268dc7c2c9ef76775eb3f0684cc
Daniil Pinchuk <JeetWork168@gmail.com>
2024-02-20 16:47:08 +0300

    add:router

--- src/router/global-route.tsx
+++ src/router/global-route.tsx
@@ -1,15 +1,32 @@
-
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { MainPage } from '../pages';
+import { Auth } from '@pages/auth';
+import { ProtectedRoute } from '@utils/protected-route';
+import { useAppSelector } from '@hooks/typed-react-redux-hooks';
+import LoginError from '@components/result/login-error';
+import RegError from '@components/result/reg-error';
+import RegSucsess from '@components/result/reg-sucsess';
 
 const GlobalRoute = () => {
+    const jwt = useAppSelector((state) => state.auth.jwt);
+    console.log(localStorage);
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/' element={<MainPage />} />
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route path='/auth' element={<Auth />} />
+            <Route
+                path='/main'
+                element={
+                    <ProtectedRoute>
+                        <MainPage />
+                    </ProtectedRoute>
+                }
+            />
+            <Route path='/result/success' element={<RegSucsess />} />
+            <Route path='/result/error-user-exist' element={<RegError type={true} />} />
+            <Route path='/result/error' element={<RegError type={false} />} />
+            <Route path='/result/error-login' element={<LoginError />} />
+            <Route path='/' element={<Navigate to='/main' replace />} />
+        </Routes>
     );
 };
-
 export default GlobalRoute;
