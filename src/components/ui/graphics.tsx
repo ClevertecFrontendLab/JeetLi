5b8a2888075a2212196e63507270fcf5d21527e7
Daniil Pinchuk <JeetWork168@gmail.com>
2024-02-20 16:59:27 +0300

    add: graphics component from img component

--- src/components/ui/graphics.tsx
+++ src/components/ui/graphics.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import CleverfitURL from '../../assets/img/Cleverfit.svg';
 import FitURL from '../../assets/img/fit.svg';
 import ExitURL from '../../assets/img/Exit.svg';
+import warnIcon from '../../assets/img/warnIcon.png';
 import { ExitIconProps } from 'src/models/types';
+import errorIcon from '../../assets/img/errorIcon.png';
+import sucseccIcon from '../../assets/img/successIcon.png';
+
 export const CleverfitLogo: React.FC = () => (
     <img className='img-cleverfit' width={'100%'} src={CleverfitURL} alt='Cleverfit Logo' />
 );
@@ -19,5 +23,35 @@ export const ExitIcon: React.FC<ExitIconProps> = ({ style }) => (
         height={'14.29px'}
         src={ExitURL}
         alt='Exit icon'
+    />
+);
+export const WarnIcon: React.FC<ExitIconProps> = ({ style }) => (
+    <img
+        src={warnIcon}
+        alt='Warn icon'
+        style={style}
+        className='result-icon-warn'
+        width={'70px'}
+        height={'61.25px'}
+    />
+);
+export const ErrorIcon: React.FC<ExitIconProps> = ({ style }) => (
+    <img
+        src={errorIcon}
+        alt='Error icon'
+        style={style}
+        className='result-icon-warn'
+        width={'70px'}
+        height={'61.25px'}
+    />
+);
+export const SucseccIcon: React.FC<ExitIconProps> = ({ style }) => (
+    <img
+        src={sucseccIcon}
+        alt='Sucsecc icon'
+        style={style}
+        className='result-icon-warn'
+        width={'70px'}
+        height={'61.25px'}
     />
 );
